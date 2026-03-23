65f6b3ae40af4e05c44c4e4690c1c0d86606f40c
hyojeong0407 <hyogomushroom562@gmail.com>
2026-03-23 22:25:06 +0900

    메인화면 틀 설계

--- safelogix/src/App.tsx
+++ safelogix/src/App.tsx
@@ -1,11 +1,27 @@
 import './App.css'
+import safelogixLogo from './assets/SafeLogiXlogo.png'
+import secure from './assets/Secure.png'
+import document from './assets/Document.png'
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <div className="app">
+      <img 
+        src={safelogixLogo} 
+        alt="SafeLogiX Logo" 
+        className="img-logo" 
+      />
+      <div className="icon-box secure-pos">
+        <img src={secure} alt="Secure" className="img-secure" />
+        <p className="icon-text">감시</p>
+      </div>
+
+      <div className="icon-box document-pos">
+        <img src={document} alt="Document" className="img-document" />
+        <p className="icon-text">문서</p>
+      </div>
+
+    </div>
   )
 }
 
