5b3b3d86942cbc4ce5b11f2bb4bddba8e1b84887
hyojeong0407 <hyogomushroom562@gmail.com>
2026-04-02 23:22:40 +0900

    물류 현황 화면 설계로 인한 주석 설정

--- safelogix/src/App.tsx
+++ safelogix/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
-import Cctv from './component/Cctv'
+// import Cctv from './component/Cctv'
+import Logistics from './component/Logistics'
 // import safelogixLogo from './assets/SafeLogiXlogo.png'
 // import secure from './assets/Secure.png'
 // import document from './assets/Document.png'
@@ -22,7 +23,8 @@ function App() {
         <p className="icon-text">문서</p>
       </div> */}
 
-      <Cctv />
+      {/* <Cctv /> */}
+      <Logistics />
 
     </div>
   )
