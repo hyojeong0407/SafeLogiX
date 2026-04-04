ce24201144f507c581813d0cc29b152a89082be8
hyojeong0407 <hyogomushroom562@gmail.com>
2026-04-04 22:43:41 +0900

    화면 연결(메인 화면, CCTV, 물류 현황)

--- safelogix/src/App.tsx
+++ safelogix/src/App.tsx
@@ -1,31 +1,40 @@
+import { useState } from 'react'
 import './App.css'
 import Cctv from './component/Cctv'
-// import Logistics from './component/Logistics'
-// import safelogixLogo from './assets/SafeLogiXlogo.png'
-// import secure from './assets/Secure.png'
-// import document from './assets/Document.png'
+import Logistics from './component/Logistics'
+import safelogixLogo from './assets/SafeLogiXlogo.png'
+import secure from './assets/Secure.png'
+import document from './assets/Document.png'
+
+type View = 'home' | 'cctv' | 'logistics'
 
 function App() {
+  const [view, setView] = useState<View>('home')
+
   return (
     <div className="app">
-      {/* <img 
-        src={safelogixLogo} 
-        alt="SafeLogiX Logo" 
-        className="img-logo" 
-      />
-      <div className="icon-box secure-pos">
-        <img src={secure} alt="Secure" className="img-secure" />
-        <p className="icon-text">감시</p>
-      </div>
+        {view === 'home' && (
+          <>
+            <img
+              src={safelogixLogo}
+              alt="SafeLogiX Logo"
+              className="img-logo"
+            />
 
-      <div className="icon-box document-pos">
-        <img src={document} alt="Document" className="img-document" />
-        <p className="icon-text">문서</p>
-      </div> */}
+            <div className="icon-box secure-pos" onClick={() => setView('cctv')}>
+              <img src={secure} alt="Secure" className="img-secure" />
+              <p className="icon-text">감시</p>
+            </div>
 
-      <Cctv />
-      {/* <Logistics /> */}
+            <div className="icon-box document-pos" onClick={() => setView('logistics')}>
+              <img src={document} alt="Document" className="img-document" />
+              <p className="icon-text">문서</p>
+            </div>
+          </>
+        )}
 
+        {view === 'cctv' && <Cctv onNavigate={setView} />}
+        {view === 'logistics' && <Logistics onNavigate={setView} />}
     </div>
   )
 }
